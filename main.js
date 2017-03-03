5186db56920d6e97531ea10b8dd17d9d16885491
lrobinsonco <lrobinsonco@gmail.com>
2017-03-03 11:18:26 -0700

    json call

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-  
+  var settings = {
+ "async": true,
+ "crossDomain": true,
+ "url": "https://www.reddit.com/r/Showerthoughts/new.json",
+ "method": "GET",
+ "headers": {
+   "cache-control": "no-cache",
+   "postman-token": "52c8bf40-feec-f7b5-a4fc-38f8632efe98"
+ }
+}
+
+$.ajax(settings).done(function (response) {
+ console.log(response);
+});
 });
